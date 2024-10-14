2b5225d0435ffe0edca8982940801e6912f724c1
ramiro <ramiroadal626@gmail.com>
2024-10-14 20:15:29 -0300

    integracion de biblioteca sfml

--- main.cpp
+++ main.cpp
@@ -1,3 +1,30 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+
+    sf::RenderWindow window(sf::VideoMode(800, 800), "SFML works!");
+    sf::CircleShape shape(400.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if(event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
+
+/*
 #include <iostream>
 
 using namespace std;
@@ -7,3 +34,4 @@ int main()
     cout << "Hello world!" << endl;
     return 0;
 }
+*/
