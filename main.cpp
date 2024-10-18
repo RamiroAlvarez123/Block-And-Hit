cd2f0cfff9e4d10742b318af2373b73a99da500b
ramiro <ramiroadal626@gmail.com>
2024-10-18 19:37:51 -0300

    agregado de background

--- main.cpp
+++ main.cpp
@@ -1,37 +1,43 @@
+#include <iostream>
 #include <SFML/Graphics.hpp>
 
-int main()
-{
+sf::Texture backgroundTexture;
+sf::Sprite background;
 
-    sf::RenderWindow window(sf::VideoMode(800, 800), "SFML works!");
-    sf::CircleShape shape(400.f);
-    shape.setFillColor(sf::Color::Green);
+int main(){
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if(event.type == sf::Event::Closed)
-                window.close();
-        }
+    /** CARGA DE LA IMAGEN DE FONDO **/
 
-        window.clear();
-        window.draw(shape);
-        window.display();
+    // carga la imagen con el path dado
+    if(!backgroundTexture.loadFromFile("imgs/interfaz.png")){
+        std::cout<<"Error al cargar la textura\n";
     }
 
-    return 0;
+    // asigna la imagen al objeto background y lo posiciona en las coordenadas (0, 0)
+   background.setTexture(backgroundTexture);
+   background.setPosition(0,0);
+
+    // crea una ventana de tamaño 530x606, de titulo "2048" y que no permite la modificacion del tamaño
+   sf::RenderWindow window(sf::VideoMode(530,606), "2048", sf::Style::Titlebar | sf::Style::Close);
+   window.setFramerateLimit(60);
+
+   //bucle que se ejecuta mientras la ventana este abierta
+   while(window.isOpen()){
+
+    //procesa los posibles eventos de la ventana
+    sf::Event event;
+    while(window.pollEvent(event)){
+        //cierra la ventana y el programa
+        if(event.type==sf::Event::Closed) window.close();
+    }
+
+    //limpia la ventana
+    window.clear();
+   //dibuja la imagen en la ventana
+    window.draw(background);
+    //muestra el contenido
+    window.display();
+   }
+
+   return 0;
 }
-
-/*
-#include <iostream>
-
-using namespace std;
-
-int main()
-{
-    cout << "Hello world!" << endl;
-    return 0;
-}
-*/
